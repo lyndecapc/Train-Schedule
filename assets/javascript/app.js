78cdcb5f171d0b6dda7bedcbe626cdccb5e6b5bd
Lynde Capogreco <lyndecapc@gmail.com>
2020-01-28 20:31:21 -0600

    firebase initialization

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyD-LCDhGu0ydoez0zxR0IhZsNu0PZeWCxY",
+    authDomain: "trainscheduler-a9d23.firebaseapp.com",
+    databaseURL: "https://trainscheduler-a9d23.firebaseio.com",
+    projectId: "trainscheduler-a9d23",
+    storageBucket: "trainscheduler-a9d23.appspot.com",
+    messagingSenderId: "650818893319",
+    appId: "1:650818893319:web:b2fdcc0217e76dab479ca2"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  // Create a variable to reference the database
+  var database = firebase.database();
